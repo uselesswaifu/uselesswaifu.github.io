6c6779c9d4c5b4807bb20d6fe6ebad96433cddcd
Nora <endercreeper4ut@gmail.com>
2020-11-09 21:40:19 -0500

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -9,11 +9,11 @@ weather.temperature = {
 };
 
 // Change to 'F' for Fahrenheit
-var tempUnit = 'C';
+var tempUnit = 'F';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = '4990729';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 42.331429;
+    let longitude = -83.045753;
 
     getWeather(latitude, longitude);
 }
